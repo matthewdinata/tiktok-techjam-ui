15b972a7c83fde65a9e2d1883fd36f43a989e461
mattdinata <matt.dinata@gmail.com>
2024-06-20 22:41:37 +0800

    init: set up linter and precommit hook

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"airbnb",
+		"prettier",
+		"next/core-web-vitals",
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: ["react", "prettier"],
+	rules: {
+		"prettier/prettier": "error",
+		"import/extensions": [
+			"error",
+			"ignorePackages",
+			{
+				js: "never",
+				jsx: "never",
+				ts: "never",
+				tsx: "never",
+			},
+		],
+		"react/jsx-filename-extension": [
+			2,
+			{ extensions: [".js", ".jsx", ".ts", ".tsx"] },
+		],
+		"react/prop-types": "off",
+		"react/require-default-props": "off",
+	},
+};
