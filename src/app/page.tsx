9ac9f574f17d70ff4bf5b08f707b177b95103bc3
mattdinata <matt.dinata@gmail.com>
2024-06-23 12:54:48 +0800

    feat: show session user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 import SignOutButton from "./components/sign-out-button";
 
-export default function Home() {
+export default async function LandingPage() {
+	const session = await auth();
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -34,18 +36,11 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-				<Image
-					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
+			<div className="mt-8 mb-4 text-3xl text-center font-medium">
+				Hello, {session?.user.name}!
 			</div>
 
-			<div className="py-12">
+			<div className="py-8">
 				<SignOutButton
 					signOut={async () => {
 						"use server";
