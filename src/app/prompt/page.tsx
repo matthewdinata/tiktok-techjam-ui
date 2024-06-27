78782da171c7becbeb85ee2341cb17c56d4e1c90
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2024-06-28 00:12:52 +0700

    feat: make next button sticky to bottom bar

--- src/app/prompt/page.tsx
+++ src/app/prompt/page.tsx
@@ -11,19 +11,19 @@ import MomentsColumn, { type Card } from "./components/moments-column";
 export default function PromptPage() {
 	const [cards, setCards] = useState<Card[]>([]);
 	return (
-		<div className="bg-neutral-900 h-max flex flex-col min-h-full">
+		<div className="bg-neutral-900 h-max flex flex-col min-h-full pb-20">
 			<div className="p-6 flex flex-col gap-6 w-full">
 				<Link href="/" className="w-full flex text-white">
 					<BiChevronLeft fontSize={24} className="cursor-pointer" />
 				</Link>
 			</div>
-			<div className="flex flex-col items-center mt-6">
+			<div className="flex flex-col items-center mt-2">
 				<h1 className="text-2xl font-bold px-10 text-center text-white leading-8">
 					Tell us crucial moments you want to include
 				</h1>
 			</div>
 			<MomentsColumn cards={cards} setCards={setCards} />
-			<div className="mb-16 mt-auto w-full flex justify-center">
+			<div className="fixed bottom-4 w-full flex justify-center">
 				<Button
 					className={`bg-rose-600 hover:bg-rose-700 focus:bg-rose-700 w-4/5 mt-5 ${cards.length === 0 ? "hidden" : ""}`}
 				>
