70dd679cdd9340cddd1775cc271923e0363cb075
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2024-07-03 01:09:56 +0700

    feat: implement edit result page

---
+++ src/app/result/edit/utils.tsx
@@ -0,0 +1,18 @@
+import { RefObject } from "react";
+
+const captureVideoFrame = (videoRef: RefObject<HTMLVideoElement>) => {
+	if (videoRef.current) {
+		const canvas = document.createElement("canvas");
+		canvas.width = videoRef.current.videoWidth;
+		canvas.height = videoRef.current.videoHeight;
+		const ctx = canvas.getContext("2d");
+		if (ctx) {
+			ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+		}
+		return canvas.toDataURL("image/jpeg");
+	}
+	return null;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { captureVideoFrame };
