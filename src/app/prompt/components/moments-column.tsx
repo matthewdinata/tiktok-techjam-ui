8a99e65331ff6661bd9b08c4689948db2775873f
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2024-07-06 16:22:55 +0700

    feat: remove drag and drop + add prompt limit

--- src/app/prompt/components/moments-column.tsx
+++ src/app/prompt/components/moments-column.tsx
@@ -28,28 +28,15 @@ function DropIndicator({ beforeId }: DropIndicatorProps) {
 type CardProps = {
 	title: string;
 	id: string;
-	handleDragStart: (e: React.DragEvent<HTMLDivElement>, card: Card) => void;
 	handleDelete: (id: string) => void;
 };
 
-function CardComponent({
-	title,
-	id,
-	handleDragStart,
-	handleDelete,
-}: CardProps) {
+function CardComponent({ title, id, handleDelete }: CardProps) {
 	return (
 		<>
 			<DropIndicator beforeId={id} />
 			<motion.div
 				layout
-				draggable="true"
-				onDragStart={(e: unknown) =>
-					handleDragStart(e as React.DragEvent<HTMLDivElement>, {
-						title,
-						id,
-					})
-				}
 				className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 mb-2 active:cursor-grabbing flex justify-between items-center gap-2 group"
 			>
 				<p className="text-sm text-neutral-100">{title}</p>
@@ -73,6 +60,7 @@ type AddCardProps = {
 function AddCard({ cards, setCards }: AddCardProps) {
 	const [text, setText] = useState("");
 	const [adding, setAdding] = useState(true);
+	const allowAdd = cards.length < 4;
 
 	const handleClose = () => {
 		if (cards.length !== 0) setAdding(false);
@@ -92,6 +80,13 @@ function AddCard({ cards, setCards }: AddCardProps) {
 		setText("");
 		setAdding(false);
 	};
+
+	if (!allowAdd)
+		return (
+			<p className="text-center text-rose-500 text-sm">
+				You have reach the moment limit.
+			</p>
+		);
 
 	return (
 		<>
@@ -139,110 +134,17 @@ type ColumnProps = {
 };
 
 export default function MomentsColumn({ cards, setCards }: ColumnProps) {
-	const handleDragStart = (
-		e: React.DragEvent<HTMLDivElement>,
-		card: Card
-	) => {
-		e.dataTransfer.setData("cardId", card.id);
-	};
-
 	const handleDelete = (id: string) => {
 		setCards((prevCards) => prevCards.filter((card) => card.id !== id));
 	};
 
-	const getNearestIndicator = (
-		e: React.DragEvent<HTMLDivElement>,
-		indicators: HTMLElement[]
-	) => {
-		const DISTANCE_OFFSET = 50;
-		const el = indicators.reduce(
-			(closest, child) => {
-				const box = child.getBoundingClientRect();
-				const offset = e.clientY - (box.top + DISTANCE_OFFSET);
-				if (offset < 0 && offset > closest.offset) {
-					return { offset, element: child };
-				}
-				return closest;
-			},
-			{
-				offset: Number.NEGATIVE_INFINITY,
-				element: indicators[indicators.length - 1],
-			}
-		);
-		return el;
-	};
-
-	const getIndicators = () =>
-		Array.from(document.querySelectorAll(`[data-before]`)) as HTMLElement[];
-
-	const clearHighlights = (els?: HTMLElement[]) => {
-		const indicators = els || getIndicators();
-		indicators.forEach((i) => {
-			// eslint-disable-next-line no-param-reassign
-			i.style.opacity = "0";
-		});
-	};
-
-	const highlightIndicator = (e: React.DragEvent<HTMLDivElement>) => {
-		const indicators = getIndicators();
-		clearHighlights(indicators);
-		const el = getNearestIndicator(e, indicators);
-		el.element.style.opacity = "1";
-	};
-
-	const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-		e.preventDefault();
-		highlightIndicator(e);
-	};
-
-	const handleDragLeave = () => {
-		clearHighlights();
-	};
-
-	const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-		clearHighlights();
-
-		const cardId = e.dataTransfer.getData("cardId");
-		const indicators = getIndicators();
-		const { element } = getNearestIndicator(e, indicators);
-
-		const before = element.dataset.before || "-1";
-
-		if (before !== cardId) {
-			let copy = [...cards];
-
-			const cardToMove = copy.find((c) => c.id === cardId);
-			if (!cardToMove) return;
-
-			copy = copy.filter((c) => c.id !== cardId);
-
-			const moveToBack = before === "-1";
-
-			if (moveToBack) {
-				copy.push(cardToMove);
-			} else {
-				const insertAtIndex = copy.findIndex((c) => c.id === before);
-				if (insertAtIndex === -1) return;
-				copy.splice(insertAtIndex, 0, cardToMove);
-			}
-
-			setCards(copy);
-		}
-	};
-
 	return (
 		<div className="w-full shrink-0 px-10 mt-10">
-			<div
-				onDragOver={handleDragOver}
-				onDragLeave={handleDragLeave}
-				onDrop={handleDragEnd}
-				className="h-full w-full transition-colors bg-neutral-800/0"
-			>
+			<div className="h-full w-full transition-colors bg-neutral-800/0">
 				{cards.map((c) => (
 					<CardComponent
 						key={c.id}
 						{...c}
-						handleDragStart={handleDragStart}
 						handleDelete={handleDelete}
 					/>
 				))}
